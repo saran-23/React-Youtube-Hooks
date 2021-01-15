253094cdd159913544e059aa450c558ce132091f
Saran <romansaran446@gmail.com>
2021-01-15 09:28:36 +0530

    refactoring SearchBar component using HOOKS

--- Youtube-Hooks/src/Components/SearchBar.js
+++ Youtube-Hooks/src/Components/SearchBar.js
@@ -1,33 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-class SearchBar extends React.Component {
-    state = {term: ''};
+const SearchBar = ({onFormSubmit}) => {
+    const[term, setTerm] = useState('');
+
+
 
 // used to enter in the input field 
-   onInputChange =event => {
-        this.setState({term: event.target.value
-        });
+   const onInputChange =event => {
+       setTerm(event.target.value);
     };
 
-    onFormSubmit = event => {
-        event.preventDefault(); //prevent from refeshing the page
+   const onSubmit = event => {
+    event.preventDefault(); //prevent from refeshing the page
 
-        this.props.onFormSubmit(this.state.term)
-    };
-    render(){
-        return (
-            <div className="search-bar ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                <div className="field">
-                    <label>Video Search</label>
-                    <input type="text"
-                     value={this.state.term} 
-                     onChange={this.onInputChange}
-                     />
-                </div>
-                </form>
-            </div>
-        );
-     }
-}
+    onFormSubmit(term);
+};
+return (
+    <div className="search-bar ui segment">
+        <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+            <label>Video Search</label>
+            <input type="text"
+             value={term} 
+             onChange={onInputChange}
+             />
+        </div>
+        </form>
+    </div>
+);
+
+};
+
 export default SearchBar;
+
+
+
+
+
+
+
